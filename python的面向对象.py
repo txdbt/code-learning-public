ed423865eaeee40e1933cb98d43224c07cc7eeaf
Txdbt <2518587521@qq.com>
2025-07-18 23:39:21 +0800

    Add files via upload

--- python的面向对象.py
+++ python的面向对象.py
@@ -1,4 +1,4 @@
-class Employee:
+"""class Employee:
     def __init__(self,emp_id,name,age,monthly_salary):
         self.emp_id = emp_id
         self.name = name
@@ -22,4 +22,34 @@ employee = Employee("E001","mike",30,8000)
 
 employer = Employer(5000)
 
-employer.pay_salary(employee)
+employer.pay_salary(employee)"""
+
+
+class Phone:
+    def __init__(self,brand:str,color:str,prize:int) -> None:
+        self.color = color
+        self.prize = prize
+        self.turned_on: bool = False
+
+    def turn_on(self) -> None:                                      #創建方法
+        if self.turned_on:
+            print(f"{self.brand} has already turn on")
+        else:
+            self.turned_on = True
+            print(f"{self.brand} is now turn on")
+
+
+    def turn_off(self) -> None:                                      #創建方法
+        if self.turned_on:
+            self.turned_on = False
+            print(f"{self.brand} is now turn off")
+        else:
+            print(f"{self.brand} is already turn off")
+
+
+
+
+
+
+samsung: Phone = Phone('samsung','blue',10000)
+print(samsung)
